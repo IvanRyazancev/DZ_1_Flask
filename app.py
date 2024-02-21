7b1c5a3a019b0a4c4cda0e7fa474c96cb23c7715
ryazancev.azov <ryazancev.azov@gmail.com>
2024-02-22 00:54:49 +0300

    Завершение дз

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/category/')
+def category():
+    return render_template('category.html')
+
+
+@app.route('/product/')
+def product():
+    return render_template('product.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
